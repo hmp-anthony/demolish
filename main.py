29d6c074f1215c9ff033c0708a533661ad790f5b
hmp-anthony <anthonygoddard962@gmail.com>
2023-11-10 20:07:16 +0000

    fleshing out main

--- main.py
+++ main.py
@@ -10,3 +10,25 @@ class Demolish:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
 
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+    def update(self):
+        self.clock.tick(FPS)
+
+    def draw(self):
+        self.screen.fill('black')
+        pg.display.flip()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    sim = Demolish()
+    sim.run()
